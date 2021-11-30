8d9497f95e5d034a8ec931b0a6d35db521f2e2da
Martin Prokes <martin.prokes@profiq.com>
2021-11-30 15:25:24 +0100

    Ik 1828 add jest code coverage to UI and node sdk martin prokes web sdk (#9)
    * Add jest test coverage job
    
    * fix the changelog formatting issue
    
    * add constant test

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,16 @@ const config = {
   verbose: false,
   testEnvironment: "jsdom",
   collectCoverageFrom: ["./lib/**/*.js"],
-  coverageReporters: ["text-summary", "html"],
+  coverageReporters: ["text", "text-summary", "json-summary"],
+  moduleDirectories: ["src", "node_modules"],
+  coverageThreshold: {
+    global: {
+      branches: 0,
+      functions: 0,
+      lines: 0,
+      statements: 0,
+    },
+  },
 };
 
 module.exports = config;

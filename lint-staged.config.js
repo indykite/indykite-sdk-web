e00cab82b91a37297462d0bb20496caa1abb29c7
Martin Prokes <martin.prokes@profiq.cz>
2022-02-16 10:29:26 +0100

    chore: add git hooks

---
+++ lint-staged.config.js
@@ -0,0 +1,6 @@
+/* eslint sonarjs/no-duplicate-string: 0 */
+module.exports = {
+  "*.{js,jsx}": ["prettier --write"],
+  "*.{css,scss,html,json,graphql}": ["prettier --write"],
+  "*.{yaml,yml}": ["prettier --write"],
+};

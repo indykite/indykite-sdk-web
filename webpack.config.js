4f299d6941adab4f43cae936d0aaf3ece282cb1d
Dušan Kováčik <dusan.kovacik@indykite.com>
2022-09-21 09:27:52 +0200

    chore: support css in webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
 const path = require("path");
 const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: path.resolve(__dirname, "lib/services/core/index.js"),
-  plugins: [new NodePolyfillPlugin()],
+  entry: [
+    path.resolve(__dirname, "lib/services/core/index.js"),
+    path.resolve(__dirname, "lib/services/core/exports/styles.scss"),
+  ],
+  plugins: [new MiniCssExtractPlugin({ filename: "styles.css" }), new NodePolyfillPlugin()],
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "iksdkweb.dist.js",
@@ -16,6 +20,11 @@ module.exports = {
         test: /\.(js)$/,
         exclude: /node_modules/,
         use: "babel-loader",
+      },
+      {
+        test: /\.(scss)$/,
+        exclude: /node_modules/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
     ],
   },
